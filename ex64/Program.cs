00079e2f569218f7808e6e03d26445dd53e94c65
Aleksandra Yudina <yudin.p.s@gmail.com>
2022-11-11 23:56:00 +0700

    feat: add solution of task 64

---
+++ ex64/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+string ListNum(int n)
+{
+    if (n == 0) return String.Empty;
+    return Convert.ToString(n) + " " + ListNum(n - 1);
+}
+
+Console.WriteLine(ListNum(number));
